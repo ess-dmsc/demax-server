4ba014f1427a1a6e0f539662932891674101485d
jeremias.hillerberg <jeremias.hillerberg@esss.se>
2018-11-29 10:38:30 +0100

    added user test

---
+++ test/user.spec.js
@@ -0,0 +1,29 @@
+const expect = require('chai').expect;
+
+const User = require('../models/user.js');
+
+describe('user', function() {
+	it('should have an email', function(done) {
+		const testUser = new User({email: 'firstname.lastname@mail.com', firstName: 'Joe'});
+
+		testUser.validate(function(err) {
+			expect(testUser.email).to.exist;
+			done();
+		});
+	});
+	it('should have a mongodb ID', function(done) {
+		const testUser = new User({});
+		testUser.validate(function(err) {
+			expect(testUser._id).to.exist;
+			done();
+		});
+	});
+	it('should have an brief summary', function(done) {
+		const testUser = new User({email: 'firstname.lastname@mail.com', firstName: 'Joe'});
+
+		testUser.validate(function(err) {
+			expect(testUser.email).to.exist;
+			done();
+		});
+	});
+});
