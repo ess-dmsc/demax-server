c30075534c4cdac36cb02433b960c6eae139c389
jeremias.hillerberg <jeremias.hillerberg@esss.se>
2019-01-21 15:45:41 +0100

    / server function test

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const Proposal = require('./models/testModel.js');
 const jwt = require('jsonwebtoken');
 const PDFDocument = require('pdfkit');
 const merge = require('easy-pdf-merge');
-let GridFsStorage = require('multer-gridfs-storage');
-let gridfs = require('gridfs-stream');
 const uploadFolder = '../files/uploads/';
 const bold = './fonts/titillium-bold-webfont.woff';
 const regular = './fonts/titillium-regular-webfont.woff';
@@ -33,7 +31,6 @@ app.get('/test', function(req, res) {res.sendfile('./public/test.html');});
 
 mongoose.Promise = global.Promise;
 const connection = mongoose.connection;
-gridfs.mongo = mongoose.mongo;
 
 //mongoose.connect("mongodb://mongodb/ess", {useNewUrlParser: true},
 mongoose.connect("mongodb://localhost:27017/ess", {useNewUrlParser: true},
@@ -48,278 +45,76 @@ mongoose.connect("mongodb://localhost:27017/ess", {useNewUrlParser: true},
 connection.on('error', console.error.bind(console, 'connection error:'));
 connection.once('open', () => {
 	console.log('successful db connection');
-	const gfs = gridfs(connection.db);
 
 	const multer = require('multer');
 
 	const storage = multer.diskStorage({
-		destination: (request, file, callback) => {
-			callback(null, '../files/uploads/');
-		},
-		filename: (request, file, callback) => {
-			callback(null, file.originalname);
-		}
+		destination: (request, file, callback) => {callback(null, './files/uploads/');},
+		filename: (request, file, callback) => {callback(null, file.originalname);}
 	});
 
 	const upload = multer({storage: storage});
 
 	app.post('/api/file/upload/needByDateAttachment', upload.single("file"), async function(request, response) {
-		try {
-			let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId},
-				{needByDateAttachment: `"./${request.file.path}"`});
-		}
-		catch(error) {
-			console.log(error);
-		}
+		try {let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId}, {needByDateAttachment: `"./${request.file.path}"`});}
+		catch(error) {console.log(error);}
 		response.send('File uploaded successfully! -> filename = ' + request.file.filename);
 	});
 
 	app.post('/api/file/upload/pbdIdReferenceAttachment', upload.single("file"), async function(request, response) {
-		try {
-			let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId},
-				{pbdIdReferenceAttachment: `"./${request.file.path}"`});
-		}
-		catch(error) {
-			console.log(error);
-		}
+		try {let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId}, {pbdIdReferenceAttachment: `"./${request.file.path}"`});}
+		catch(error) {console.log(error);}
 		response.send('File uploaded successfully! -> filename = ' + request.file.filename);
 	});
 	app.post('/api/file/upload/organismReferenceAttachment', upload.single("file"), async function(request, response) {
-		try {
-			let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId},
-				{organismReferenceAttachment: `"./${request.file.path}"`});
-		}
-		catch(error) {
-			console.log(error);
-		}
+		try {let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId}, {organismReferenceAttachment: `"./${request.file.path}"`});}
+		catch(error) {console.log(error);}
 		response.send('File uploaded successfully! -> filename = ' + request.file.filename);
 	});
 	app.post('/api/file/upload/needsPurificationSupportAttachment', upload.single("file"), async function(request, response) {
-		try {
-			let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId},
-				{needsPurificationSupportAttachment: `"./${request.file.path}"`});
-		}
-		catch(error) {
-			console.log(error);
-		}
+		try {let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId}, {needsPurificationSupportAttachment: `"./${request.file.path}"`});}
+		catch(error) {console.log(error);}
 		response.send('File uploaded successfully! -> filename = ' + request.file.filename);
 	});
 	app.post('/api/file/upload/chemicalStructureAttachment', upload.single("file"), async function(request, response) {
-		try {
-			let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId},
-				{chemicalStructureAttachment: `"./${request.file.path}"`});
-		}
-		catch(error) {
-			console.log(error);
-		}
+		try {let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId}, {chemicalStructureAttachment: `"./${request.file.path}"`});}
+		catch(error) {console.log(error);}
 		response.send('File uploaded successfully! -> filename = ' + request.file.filename);
 	});
 	app.post('/api/file/upload/moleculePreparationReferenceArticle', upload.single("file"), async function(request, response) {
-		try {
-			let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId},
-				{moleculePreparationReferenceArticle: `"./${request.file.path}"`});
-		}
-		catch(error) {
-			console.log(error);
-		}
+		try {let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId}, {moleculePreparationReferenceArticle: `"./${request.file.path}"`});}
+		catch(error) {console.log(error);}
 		response.send('File uploaded successfully! -> filename = ' + request.file.filename);
 	});
 	app.post('/api/file/upload/proposalTemplate', upload.single("file"), async function(request, response) {
-		try {
-			let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId},
-				{proposalTemplate: `"./${request.file.path}"`});
-			console.log(request.file.path);
-		}
-		catch(error) {
-			console.log(error);
-		}
+		try {let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId}, {proposalTemplate: `"./${request.file.path}"`});console.log(request.file.path);}
+		catch(error) {console.log(error);}
 		response.send('File uploaded successfully! -> filename = ' + request.file.filename);
 	});
 	app.post('/api/file/upload/generatedProposal', upload.single("file"), async function(request, response) {
-		try {
-			let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId},
-				{generatedProposal: `"./${request.file.path}"`});
-			console.log(request.file.path);
-		}
-		catch(error) {
-			console.log(error);
-		}
+		try {let doc = await Proposal.findOneAndUpdate({proposalId: request.body.proposalId}, {generatedProposal: `"./${request.file.path}"`});}
+		catch(error) {console.log(error);}
 		response.send('File uploaded successfully! -> filename = ' + request.file.filename);
-	});
-
-	app.get('/api/files/:proposalId', function(request, response) {
-
-		fs.readdir(uploadFolder, (err, files) => {
-			for(let i = 0; i < files.length; ++i) {
-				files[ i ] = files[ i ];
-			}
-
-			response.send(files);
-		});
-	});
-	app.get('/api/files/:proposalId', async function(request, response) {
-		try {
-			let proposal = await Proposal.findOne({proposalId: request.params.proposalId});
-			console.log(proposal.proposalId);
-
-			let fileArray = [
-				'test',
-				proposal.proposalTemplate,
-				proposal.needByDateAttachment,
-				proposal.pbdIdReferenceAttachment,
-				proposal.organismReferenceAttachment,
-				proposal.needsPurificationSupportAttachment,
-				proposal.chemicalStructureAttachment,
-				proposal.moleculePreparationReferenceArticle
-			];
-			console.log(fileArray);
-			response.send(fileArray);
-		} catch(error) {
-			console.log(error);
-		}
-	});
-	app.get('/api/files/:filename', function(request, response) {
-		var filename = request.params.filename;
-		response.download(uploadFolder + filename);
-	});
-	app.delete('/api/files/:filename', function(request, response) {
-		let filename = request.params.filename;
-		fs.unlink(uploadFolder + filename, (err) => {
-			if(err) throw err;
-			response.send('File successfully deleted');
-		});
-	});
-
-	app.get('/api/file/upload2', (request, response) => {
-
-		const filename = request.query.filename;
-
-		const writestream = gfs.createWriteStream({
-			filename: filename
-		});
-		fs.createReadStream(__dirname + "/uploads/" + filename).pipe(writestream);
-		writestream.on('close', (file) => {
-			response.send('Stored File: ' + file.filename + '<br><br>       File ID: ' + file._id);
-		});
-	});
-
-
-	app.post('/api/file/upload', function(request, response) {
-
-		var form = new formidable.IncomingForm();
-		form.parse(request);
-		form.on('fileBegin', async function(name, file, error) {
-			if(error || !file) {
-				response.status(404).send('File Not Found');
-				return;
-			}
-			file.path = '../files/uploads/' + file.name;
-			const filename = file.name;
-			const writestream = await gfs.createWriteStream({
-				filename: filename,
-			});
-			try {
-				fs.createReadStream("../files/uploads/" + filename).pipe(writestream);
-				writestream.on('close', (file) => {
-					console.log(file._id);
-					response.json(file._id);
-				});
-			} catch(error) {
-				return response.status(400).json({
-					error: err.message
-				});
-			}
-		});
-
-		form.on('file', function(name, file, error) {
-			if(error || !file) {
-				response.status(404).send('File Not Found');
-				return;
-			}
-			console.log('Uploaded ' + file.name);
-		});
-	});
-
-	app.get('/api/file/download', (request, response) => {
-
-		let filename = request.query.filename;
-
-		gfs.exist({
-			filename: filename
-		}, (err, file) => {
-			if(err || !file) {
-				response.status(404).send('File Not Found');
-				return;
-			}
-
-			const readstream = gfs.createReadStream({
-				filename: filename
-			});
-			readstream.pipe(response);
-		});
-	});
-
-	app.get('/api/file/delete', (request, response) => {
-
-		const filename = request.query.filename;
-
-		gfs.exist({
-			filename: filename
-		}, (err, file) => {
-			if(err || !file) {
-				response.status(404).send('File Not Found');
-				return;
-			}
-
-			gfs.remove({
-				filename: filename
-			}, (err) => {
-				if(err) response.status(500).send(err);
-				response.send('File Deleted');
-			});
-		});
-	});
-
-	app.get('/api/file/meta', (request, response) => {
-
-		const filename = request.query.filename;
-
-		gfs.exist({
-			filename: filename
-		}, (err, file) => {
-			if(err || !file) {
-				response.send('File Not Found');
-				return;
-			}
-
-			gfs.files.find({
-				filename: filename
-			}).toArray((err, files) => {
-				if(err) response.send(err);
-				response.json(files);
-			});
-		});
-	});
-
-	app.post('/api/upload', function(request, response) {
-		const form = new formidable.IncomingForm();
-		let readStream;
-
-		form.on('file', function(field, file) {
-			readStream = fs.createReadStream(file.path);
-		});
-		form.on('end', () => {
-			response.json();
-		});
-		form.parse(request);
 	});
 
 	app.use(express.static('../public'));
 
-	const paths = {
-		word: path.join(__dirname, './files/resources/', 'DEMAX_proposal_template.docx')
-	};
+	const paths = {word: path.join(__dirname, './files/resources/', 'DEMAX_proposal_template.docx')};
+
 	const fileWord = fs.readFileSync(paths.word);
+
+	app.get('/', async function(request, response){
+		try{response.sendFile('./files/resources/home.html', {root: __dirname})}
+	catch(error){response.send('Error')}})
+
+	app.get('/api', async function(request, response) {
+		try {
+			response.sendFile('./files/resources/api.html', {root: __dirname })
+		} catch(error) {
+			response.send(error);
+		}
+	});
+
 	app.get('/word/attachment', function(req, res, next) {
 		const file = fs.createReadStream(paths.word);
 		const stat = fs.statSync(paths.word);
@@ -352,7 +147,7 @@ connection.once('open', () => {
 			if(proposal.wantsBiomassDeuteration) {
 				doc.addPage().font(semibold, 18).text('(B) BIOLOGICAL DEUTERATION').moveDown().font(italic, 12).text('If the protein is to be purified by us, please remember to include a chromatogram from purification and a picture of SDS-PAGE that indicates MW and purity in your science case.').moveDown().font(semibold, 16).text('For biomass').moveDown().font(semibold, 13).text('Will user provide the organism for us to grow under deuterated conditions?').font(regular, 12).text(proposal.biomassDeuteration.organismProvidedByUser).moveDown().font(semibold, 13).text('What is the organism?').font(regular, 12).text(proposal.biomassDeuteration.organismDetails).moveDown().font(regular, 12).text(proposal.biomassDeuteration.organismReferenceAttachment).moveDown().font(semibold, 13).text('How much material do you need (indicate wet or dry mass)').font(regular, 12).text(proposal.biomassDeuteration.amountNeeded).moveDown().font(semibold, 13).text('Justify amount').font(regular, 12).text(proposal.biomassDeuteration.amountNeededMotivation).moveDown().font(semibold, 13).text('Level of deuteration required:').font(regular, 12).text(proposal.biomassDeuteration.deuterationLevelRequired).moveDown().font(semibold, 13).text('Justify level of D incorporation').font(regular, 12).text(proposal.biomassDeuteration.deuterationLevelMotivation).moveDown().font(semibold, 13).text('Other:').font(regular, 12).text(proposal.biomassDeuteration.other);
 			}
-			if(proposal.wantsProteinDeuteration) {
+			if(proposal.wantsProteinDeuteration){
 				doc.addPage().font(semibold, 16).text('For proteins').moveDown().font(semibold, 13).text('Name of molecule to be deuterated (e.g. superoxide dismutase):').font(regular, 12).text(proposal.proteinDeuteration.moleculeName).moveDown().font(semibold, 13).text('FASTA sequence or Uniprot number:').font(regular, 12).text(proposal.proteinDeuteration.moleculeIdentifier).moveDown().font(semibold, 13).text('Oligomerizarion state? (e.g. homodimer, tetramer etc.):').font(regular, 12).text(proposal.proteinDeuteration.oligomerizationState).moveDown().font(semibold, 13).text('Does the protein have any co-factors or ligands required for expression? Specify: ').font(regular, 12).text(proposal.proteinDeuteration.expressionRequirements).font(semibold, 13).text('Origin of molecules (e.g. human, E. coli, S. cerevisiae):').font(regular, 12).text(proposal.proteinDeuteration.moleculeOrigin).moveDown().font(semibold, 13).text('Will you provide an expression plasmid?').font(regular, 12).text(proposal.proteinDeuteration.expressionPlasmidProvidedByUser).moveDown().font(semibold, 13).text('If “yes”, please provide details (e.g. pET31b, C-terminal His-tag, Amp selection) If “no”, we will design & order a plasmid commercially)').font(regular, 12).text(proposal.proteinDeuteration.expressionPlasmidProvidedByUserDetails).moveDown().font(semibold, 13).text('How much material do you need: ').font(regular, 12).text(proposal.proteinDeuteration.amountNeeded).moveDown().font(semibold, 13).text('Justify amount:').font(regular, 12).text(proposal.proteinDeuteration.amountNeededMotivation).moveDown().font(semibold, 13).text('Will you need DEMAX to purify the protein from deuterated biomass?').font(regular, 12).text(proposal.proteinDeuteration.needsPurificationSupport).moveDown().font(semibold, 13).text('Has expression been done for the unlabeled protein?').font(regular, 12).text(proposal.proteinDeuteration.hasDoneUnlabeledProteinExpression).moveDown().font(semibold, 13).text('Typical yield:').font(regular, 12).text(proposal.proteinDeuteration.typicalYield).moveDown().font(semibold, 13).text('Have you been able to purify the unlabeled protein?').font(italic, 12).text('Please include chromatogram & image of SDS-PAGE in proposal.').font(regular, 12).text(proposal.proteinDeuteration.hasPurifiedUnlabeledProtein).moveDown().font(semibold, 13).text('Have you tried to deuterate the protein yourself, even in small scale?').font(regular, 12).text(proposal.proteinDeuteration.hasProteinDeuterationExperience).moveDown().font(semibold, 13).text('Results?:').font(regular, 12).text(proposal.proteinDeuteration.proteinDeuterationResults).moveDown().font(semibold, 13).text('Other:').font(regular, 12).text(proposal.proteinDeuteration.other);
 			}
 			if(proposal.biomassDeuteration || proposal.wantsProteinDeuteration) {
@@ -362,8 +157,8 @@ connection.once('open', () => {
 				doc.addPage().font(semibold, 18).text('(C) CHEMICAL DEUTERATION').moveDown().font(semibold, 13).text('Molecule/s to be deuterated (name):').font(regular, 12).text(proposal.chemicalDeuteration.moleculeName).moveDown().font(semibold, 13).text('Amount of material required (mass):').font(regular, 12).text(proposal.chemicalDeuteration.amount).moveDown().font(semibold, 13).text('Justify amount:').font(regular, 12).text(proposal.chemicalDeuteration.amountMotivation).moveDown().font(semibold, 13).text('Indicate percentage and location of deuteration:').font(regular, 12).text(proposal.chemicalDeuteration.deuterationLocationAndPercentege).moveDown().font(semibold, 13).text('Justify level of deuteration:').font(regular, 12).text(proposal.chemicalDeuteration.deuterationLevelMotivation).moveDown().font(semibold, 13).text('Has this molecule (or an unlabeled/isotopic analogue) been prepared by yourself or others?').font(regular, 12).text(proposal.chemicalDeuteration.hasPreparedMolecule).moveDown();
 			}
 			doc.save();
-			doc.pipe(fs.createWriteStream('../files/generated/' + proposal.proposalId + '_generatedProposal.pdf'));
-			let generatedDoc = doc.pipe(fs.createWriteStream('../files/generated/' + proposal.proposalId + '.pdf'));
+			doc.pipe(fs.createWriteStream('./files/generated/' + proposal.proposalId + '_generatedProposal.pdf'));
+			let generatedDoc = doc.pipe(fs.createWriteStream('./files/generated/' + proposal.proposalId + '.pdf'));
 			doc.end();
 			doc.pipe(response);
 
@@ -389,20 +184,20 @@ connection.once('open', () => {
 			files.push(proposal.chemicalStructureAttachment);
 			files.push(proposal.moleculePreparationReferenceArticle);
 			console.log(typeof( files ));
-			merge(files, `"../files/merged/${proposal.proposalId}.pdf"`,
+			merge(files, `"./files/merged/${proposal.proposalId}.pdf"`,
 				function(error) {
 					if(error) {
 						console.log(error);
 						return error;
 					}
-					const file = fs.createReadStream("../files/merged/" + proposal.proposalId + '.pdf');
-					const stat = fs.statSync("../files/merged/" + proposal.proposalId + '.pdf');
+					const file = fs.createReadStream("./files/merged/" + proposal.proposalId + '.pdf');
+					const stat = fs.statSync("./files/merged/" + proposal.proposalId + '.pdf');
 					response.setHeader('Content-Length', stat.size);
 					response.setHeader('Content-Type', 'application/pdf');
 					response.setHeader('Content-Disposition', 'attachment; filename=' + proposal.proposalId + '.pdf');
 					file.pipe(response);
 				});
-			await proposal.update({mergedProposal: `"../files/merged/${proposal.proposalId}.pdf"`});
+			await proposal.update({mergedProposal: `"./files/merged/${proposal.proposalId}.pdf"`});
 		} catch(error) {
 			console.log(error);
 		}
@@ -582,14 +377,6 @@ connection.once('open', () => {
 			return response.status(400).json({
 				error: err.message
 			});
-		}
-	});
-
-	app.get('/api', async function(request, response) {
-		try {
-			response.sendFile('./files/resources/api.html', {root: __dirname});
-		} catch(error) {
-			response.send(error);
 		}
 	});
 
