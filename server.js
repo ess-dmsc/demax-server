6e17a736c35a1a880f1cc3de8a5e515a46cb9f2c
Jeremias <40340942+localghost-8080@users.noreply.github.com>
2019-01-29 14:03:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -234,16 +234,50 @@ connection.once('open', () => {
 		}
 	});
 
-	app.post('/api/users/register', async function(request, response) {
+		app.post('/api/users/register', async function(request, response) {
+
+		await findUserByEmail(request.body.email);
+		const userData ={
+			email: request.body.email,
+			password: request.body.password,
+			firstName: request.body.firstName,
+			lastName: request.body.lastName,
+			phone: request.body.phone,
+			industry: request.body.industry,
+			employer: request.body.employer,
+			jobTitle: request.body.jobTitle,
+			hasConsentedToGdpr: request.body.hasConsentedToGdpr,
+			hasConsentedToEmails: request.body.hasConsentedToEmails,
+		};
+
 		try {
-			const obj = await new User(request.body).save();
-			response.status(201).json(obj);
+			const newUser = await new User(userData);
+			newUser.save(function(error){
+				console.log(newUser);
+				if(error){
+					throw error;
+				}
+			})
+			response.status(201).json(newUser);
 		} catch(err) {
 			return response.status(400).json({
 				error: err.message
 			});
 		}
 	});
+
+	function findUserByEmail(email){
+		if(email){
+			return new Promise((resolve, reject) => {
+				User.findOne({ email: email })
+				.exec((error, document) => {
+					if (error) return reject(error)
+					if (document) return reject(new Error('Email already exists. Please enter another email.'))
+					else return resolve(email)
+				})
+			})
+		}
+	}
 
 	app.post('/api/users', async function(request, response) {
 		try {
