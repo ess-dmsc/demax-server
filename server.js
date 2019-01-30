0e946b395892dc408618963504894575190391fe
Jeremias <40340942+localghost-8080@users.noreply.github.com>
2019-01-30 13:26:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose.Promise = global.Promise;
 const connection = mongoose.connection;
 
 mongoose.connect("mongodb://mongodb/ess", {useNewUrlParser: true},
-		 function(error, client) {
+	function(error, client) {
 		if(error) {
 			console.log(error);
 			process.exit(1);
@@ -64,19 +64,20 @@ connection.once('open', () => {
 		response.send('File uploaded successfully! -> Filename = ' + request.file.filename);
 	});
 
-	app.get('/api/file/all', async function (request, response) {
+	app.get('/api/file/all', async function(request, response) {
 
-		try{
+		try {
 			fs.readdir(__basedir + '/files/uploads/', (err, files) => {
 				for(let i = 0; i < files.length; ++i) {
 					files[ i ] = files[ i ];
 				}
 				response.send(files);
 			});
-		}catch(error){
+		} catch(error) {
 			return response.status(400).json({
 				error: error.message
-			});		}
+			});
+		}
 	});
 
 	app.get('/api/file/:filename', (request, response) => {
@@ -111,14 +112,20 @@ connection.once('open', () => {
 		}
 	});
 
-		app.get('/word/attachment', function(request, response, next) {
+	app.get('/word/attachment', function(request, response, next) {
 		response.download(__basedir + '/files/resources/' + 'DEMAX_proposal_template.docx');
 	});
 
 	app.get('/api/pdf/:id', async function(request, response) {
 		const doc = new PDFDocument();
+
+
 		try {
+			console.log('Request.params.id: ' + request.params.id);
 			let proposal = await Proposal.findOne({proposalId: request.params.id});
+
+			console.log('ProposalID: ' + proposal.proposalId);
+
 			let filename = proposal.proposalId + '';
 			filename = encodeURIComponent(filename) + '.pdf';
 
@@ -127,13 +134,69 @@ connection.once('open', () => {
 			response.setHeader('Access-Control-Allow-Origin', '*');
 
 			doc.info[ 'Title' ] = proposal.proposalId + '.pdf';
-			doc.info[ 'Author' ] = proposal.mainProposerFirstName + ' ' + proposal.mainProposerLastName;
+			doc.info[ 'Author' ] = proposal.mainProposer.firstName + ' ' + proposal.mainProposer.lastName;
 
-			doc.font(semibold, 25).text(proposal.experimentTitle).moveDown().fontSize(14).text('Brief summary: ').font(regular, 12).text(proposal.briefSummary).moveDown().font(semibold, 14).text('Main proposer:').font(regular, 12).text(proposal.mainProposer.firstName + ' ' + proposal.mainProposer.lastName).text(proposal.mainProposer.employer).text(proposal.mainProposer.title).moveDown().text(proposal.mainProposer.email).text(proposal.mainProposer.phone).moveDown().font(semibold, 14).text('Co-proposers:').moveDown().font(semibold, 14).text('Links with industry:').font(regular, 12).text(proposal.linksWithIndustry).moveDown().font(semibold, 14).text('Are any of the co-proposers students?').font(regular, 12).text(proposal.coProposerStudents).moveDown().font(semibold, 14).text('Does the proposal work towards a students degree?').font(regular, 12).text(proposal.workTowardsStudentsDegree).moveDown().font(semibold, 14).text('Need-by-date: ').font(regular, 12).text(proposal.needByDate).moveDown().font(semibold, 14).text('Motivation: ').font(regular, 12).text(proposal.needByDateMotivation);
+			doc.font(semibold, 25).text(proposal.experimentTitle).moveDown()
+
+			.fontSize(14).text('Brief summary: ').font(regular, 12).text(proposal.briefSummary).moveDown()
+
+			.font(semibold, 14).text('Main proposer:')
+			.font(regular, 12).text(proposal.mainProposer.firstName + ' ' + proposal.mainProposer.lastName).moveDown()
+			.font(semibold, 12).text('Contact information')
+			.text(proposal.mainProposer.email)
+			.text(proposal.mainProposer.phone).moveDown()
+			.font(semibold, 12).text('Affiliation')
+			.text(proposal.mainProposer.industry)
+			.text(proposal.mainProposer.employer)
+			.text(proposal.mainProposer.jobTitle).moveDown()
+
+			.font(semibold, 14).text('Co-proposers:').moveDown()
+
+			.font(semibold, 14).text('Indicators').moveDown()
+
+			.font(semibold, 12).text('Links with industry:')
+			.font(regular, 12).text(proposal.linksWithIndustry).moveDown()
+			.font(semibold, 12).text('Are any of the co-proposers students?')
+			.font(regular, 12).text(proposal.coProposerStudents).moveDown()
+			.font(semibold, 12).text('Does the proposal work towards a students degree?')
+			.font(regular, 12).text(proposal.workTowardsStudentsDegree).moveDown()
+			.font(semibold, 12).text('Need-by-date: ')
+			.font(regular, 12).text(proposal.needByDate).moveDown()
+			.font(semibold, 14).text('Motivation: ')
+			.font(regular, 12).text(proposal.needByDateMotivation).moveDown()
+
+			.font(semibold, 14).text('Required deuteration methods').moveDown()
+			.font(semibold, 12).text('(A) Crystallization')
+			.font(regular, 12).text(proposal.wantsCrystallization).moveDown
+			.font(semibold, 12).text('(B) Biological deuteration')
+			.font(regular, 12).text(proposal.wantsBiologicalDeuteration)
+			.font(regular, 12).text(proposal.wantsBiomassDeuteration)
+			.font(regular, 12).text(proposal.wantsProteinDeuteration)
+			.font(regular, 12).text(proposal.wantsOtherDeuteration)
+			.font(semibold, 12).text('(C) Chemical deuteration')
+			.font(regular, 12).text(proposal.wantsChemicalDeuteration)
 
 			if(proposal.wantsCrystallization) {
-				doc.addPage().font(semibold, 18).text('(A) CRYSTALLIZATION').moveDown().font(italic, 12).text('Required information to include in “Practical Considerations” section of your proposal: SDS-PAGE and chromatogram of protein purification to indicate yield & purity; photo of crystal.').moveDown().font(semibold, 13).text('Name of molecule to be crystallized (e.g. superoxide dismutase): ').font(regular, 12).text(proposal.crystallization.moleculeName).moveDown().font(semibold, 13).text('FASTA sequence or Uniprot number: ').font(regular, 12).text(proposal.crystallization.moleculeIdentifier).moveDown().font(semibold, 13).text('Molecular weight (kDA): ').font(regular, 12).text(proposal.crystallization.molecularWeight).moveDown().font(semibold, 13).text('PDB ID of crystal structure: ').font(regular, 12).text(proposal.crystallization.pbdId).moveDown().font(semibold, 13).text('DOI: ').font(regular, 12).text(proposal.crystallization.doi).moveDown().font(semibold, 13).text('Does the protein have any co-factors or ligands required for crystallization? Specify: ').font(regular, 12).text(proposal.crystallization.crystallizationRequirements).moveDown().font(semibold, 13).text('Known crystallization precipitant composition (incl. buffer, salt, additives, pH): ').font(regular, 12).text(proposal.crystallization.crystallizationPrecipitantComposition).moveDown().font(semibold, 13).text('What crystallization method, volume, and temperature have you used in the past?').font('Times-Roman').text('(e.g. vapour diffusion, 10µL drops, room temperature)').font(regular, 12).text(proposal.crystallization.previousCrystallizationExperience).moveDown().font(semibold, 13).text('How long do your crystals take to appear?: ').font(regular, 12).text(proposal.crystallization.estimatedCrystallizationProductionTime).moveDown().font(semibold, 13).text('What is the typical size of your crystal?').font('Times-Roman').text('(µm x µm x µm): ').font(regular, 12).text(proposal.crystallization.typicalCrystalSize).moveDown().font(semibold, 16);
-				doc.addPage().text('Details from protein preparation').moveDown().font(semibold, 13).text('Typical yield (mg per liter of culture):').font(regular, 12).text(proposal.crystallization.typicalYieldMgPerLiter).moveDown().font(semibold, 13).text('Storage conditions (e.g. stable at 4 °C or frozen at -20 °C):').font(regular, 12).text(proposal.crystallization.storageConditions).moveDown().font(semibold, 13).text('Stability:').font(regular, 12).text(proposal.crystallization.stability).moveDown().font(semibold, 13).text('What buffer is your protein in?:').font(regular, 12).text(proposal.crystallization.buffer).moveDown().font(semibold, 13).text('Is your protein partially or fully deuterated?:').font(regular, 12).text(proposal.crystallization.levelOfDeuteration).moveDown().font(semibold, 13).text('What protein concentration do you usually use for crystallization: ').font(regular, 12).text(proposal.crystallization.typicalProteinConcentrationUsed).moveDown().font(semibold, 13).text('Other:').font(regular, 12).text(proposal.crystallization.other);
+				doc.addPage()
+				.font(semibold, 18).text('(A) CRYSTALLIZATION').moveDown()
+				.font(italic, 12).text('Required information to include in “Practical Considerations” section of your proposal: SDS-PAGE and chromatogram of protein purification to indicate yield & purity; photo of crystal.').moveDown()
+				.font(semibold, 13).text('Name of molecule to be crystallized (e.g. superoxide dismutase): ').font(regular, 12).text(proposal.crystallization.moleculeName).moveDown()
+				.font(semibold, 13).text('FASTA sequence or Uniprot number: ').font(regular, 12).text(proposal.crystallization.moleculeIdentifier).moveDown()
+				.font(semibold, 13).text('Molecular weight (kDA): ').font(regular, 12).text(proposal.crystallization.molecularWeight).moveDown()
+				.font(semibold, 13).text('PDB ID of crystal structure: ').font(regular, 12).text(proposal.crystallization.pbdId).moveDown()
+				.font(semibold, 13).text('DOI: ').font(regular, 12).text(proposal.crystallization.doi).moveDown()
+				.font(semibold, 13).text('Does the protein have any co-factors or ligands required for crystallization? Specify: ').font(regular, 12).text(proposal.crystallization.crystallizationRequirements).moveDown()
+				.font(semibold, 13).text('Known crystallization precipitant composition (incl. buffer, salt, additives, pH): ').font(regular, 12).text(proposal.crystallization.crystallizationPrecipitantComposition).moveDown()
+				.font(semibold, 13).text('What crystallization method, volume, and temperature have you used in the past?').font('Times-Roman').text('(e.g. vapour diffusion, 10µL drops, room temperature)').font(regular, 12).text(proposal.crystallization.previousCrystallizationExperience).moveDown()
+				.font(semibold, 13).text('How long do your crystals take to appear?: ').font(regular, 12).text(proposal.crystallization.estimatedCrystallizationProductionTime).moveDown()
+				.font(semibold, 13).text('What is the typical size of your crystal?').font('Times-Roman').text('(µm x µm x µm): ').font(regular, 12).text(proposal.crystallization.typicalCrystalSize).moveDown().font(semibold, 16);
+				doc.addPage().text('Details from protein preparation').moveDown()
+				.font(semibold, 13).text('Typical yield (mg per liter of culture):').font(regular, 12).text(proposal.crystallization.typicalYieldMgPerLiter).moveDown()
+				.font(semibold, 13).text('Storage conditions (e.g. stable at 4 °C or frozen at -20 °C):').font(regular, 12).text(proposal.crystallization.storageConditions).moveDown()
+				.font(semibold, 13).text('Stability:').font(regular, 12).text(proposal.crystallization.stability).moveDown().font(semibold, 13).text('What buffer is your protein in?:').font(regular, 12).text(proposal.crystallization.buffer).moveDown()
+				.font(semibold, 13).text('Is your protein partially or fully deuterated?:').font(regular, 12).text(proposal.crystallization.levelOfDeuteration).moveDown()
+				.font(semibold, 13).text('What protein concentration do you usually use for crystallization: ').font(regular, 12).text(proposal.crystallization.typicalProteinConcentrationUsed)
+				.moveDown().font(semibold, 13).text('Other:').font(regular, 12).text(proposal.crystallization.other);
 			}
 			if(proposal.wantsBiomassDeuteration) {
 				doc.addPage().font(semibold, 18).text('(B) BIOLOGICAL DEUTERATION').moveDown().font(italic, 12).text('If the protein is to be purified by us, please remember to include a chromatogram from purification and a picture of SDS-PAGE that indicates MW and purity in your science case.').moveDown().font(semibold, 16).text('For biomass').moveDown().font(semibold, 13).text('Will user provide the organism for us to grow under deuterated conditions?').font(regular, 12).text(proposal.biomassDeuteration.organismProvidedByUser).moveDown().font(semibold, 13).text('What is the organism?').font(regular, 12).text(proposal.biomassDeuteration.organismDetails).moveDown().font(regular, 12).text(proposal.biomassDeuteration.organismReferenceAttachment).moveDown().font(semibold, 13).text('How much material do you need (indicate wet or dry mass)').font(regular, 12).text(proposal.biomassDeuteration.amountNeeded).moveDown().font(semibold, 13).text('Justify amount').font(regular, 12).text(proposal.biomassDeuteration.amountNeededMotivation).moveDown().font(semibold, 13).text('Level of deuteration required:').font(regular, 12).text(proposal.biomassDeuteration.deuterationLevelRequired).moveDown().font(semibold, 13).text('Justify level of D incorporation').font(regular, 12).text(proposal.biomassDeuteration.deuterationLevelMotivation).moveDown().font(semibold, 13).text('Other:').font(regular, 12).text(proposal.biomassDeuteration.other);
@@ -147,16 +210,14 @@ connection.once('open', () => {
 			if(proposal.wantsChemicalDeuteration) {
 				doc.addPage().font(semibold, 18).text('(C) CHEMICAL DEUTERATION').moveDown().font(semibold, 13).text('Molecule/s to be deuterated (name):').font(regular, 12).text(proposal.chemicalDeuteration.moleculeName).moveDown().font(semibold, 13).text('Amount of material required (mass):').font(regular, 12).text(proposal.chemicalDeuteration.amount).moveDown().font(semibold, 13).text('Justify amount:').font(regular, 12).text(proposal.chemicalDeuteration.amountMotivation).moveDown().font(semibold, 13).text('Indicate percentage and location of deuteration:').font(regular, 12).text(proposal.chemicalDeuteration.deuterationLocationAndPercentege).moveDown().font(semibold, 13).text('Justify level of deuteration:').font(regular, 12).text(proposal.chemicalDeuteration.deuterationLevelMotivation).moveDown().font(semibold, 13).text('Has this molecule (or an unlabeled/isotopic analogue) been prepared by yourself or others?').font(regular, 12).text(proposal.chemicalDeuteration.hasPreparedMolecule).moveDown();
 			}
+
 			doc.save();
 			doc.pipe(fs.createWriteStream('./files/generated/' + proposal.proposalId + '_generatedProposal.pdf'));
 			let generatedDoc = doc.pipe(fs.createWriteStream('./files/generated/' + proposal.proposalId + '.pdf'));
-
-			await Proposal.findOneAndUpdate({
-				proposalId: request.body.proposalId,
-				generatedProposal: `"${generatedDoc.path}"`
-			});
-
 			doc.end();
+
+			await Proposal.findOneAndUpdate({proposalId: request.body.proposalId}, {generatedProposal: `"${generatedDoc.path}"`});
+
 			doc.pipe(response);
 		}
 		catch(error) {
@@ -234,7 +295,7 @@ connection.once('open', () => {
 
 		await findUserByEmail(request.body.email);
 
-		const userData ={
+		const userData = {
 			email: request.body.email,
 			password: request.body.password,
 			firstName: request.body.firstName,
@@ -249,12 +310,12 @@ connection.once('open', () => {
 
 		try {
 			const newUser = await new User(userData);
-			newUser.save(function(error){
+			newUser.save(function(error) {
 				console.log(newUser);
-				if(error){
+				if(error) {
 					throw error;
 				}
-			})
+			});
 			response.status(201).json(newUser);
 		} catch(err) {
 			return response.status(400).json({
@@ -263,16 +324,15 @@ connection.once('open', () => {
 		}
 	});
 
-	function findUserByEmail(email){
-		if(email){
+	function findUserByEmail(email) {
+		if(email) {
 			return new Promise((resolve, reject) => {
-				User.findOne({ email: email })
-				.exec((error, document) => {
-					if (error) return reject(error)
-					if (document) return reject(new Error('Email already exists. Please enter another email.'))
-					else return resolve(email)
-				})
-			})
+				User.findOne({email: email}).exec((error, document) => {
+					if(error) return reject(error);
+					if(document) return reject(new Error('Email already exists. Please enter another email.'));
+					else return resolve(email);
+				});
+			});
 		}
 	}
 
@@ -328,11 +388,26 @@ connection.once('open', () => {
 
 	app.get('/api/proposals', async function(request, response) {
 		try {
-			const docs = await Proposal.find({});
-			response.status(200).json(docs);
-		} catch(err) {
+			const proposals = await Proposal.find({});
+			response.status(200).json(proposals);
+		} catch(error) {
 			return response.status(400).json({
-				error: err.message
+				error: error.message
+			});
+		}
+	});
+
+	app.get('/api/proposals/meta', async function(request, response) {
+		try {
+			const proposals = await Proposal.find({});
+			let responseArray = [];
+			for(let i in proposals){
+				responseArray.push("Proposal ID:" + proposals[i].proposalId)
+			}
+			response.status(200).json(responseArray);
+		} catch(error) {
+			return response.status(400).json({
+				error: error.message
 			});
 		}
 	});
@@ -349,12 +424,13 @@ connection.once('open', () => {
 	});
 	app.post('/api/proposals', async function(request, response) {
 		try {
-			const obj = await new Proposal(request.body).save();
-			await obj.update({proposalId: nanoid('23456789ABCDEFGHJKLMNPQRSTUVXYZ', 8)});
-			response.status(201).json(obj);
-		} catch(err) {
+			const newProposal = request.body;
+			newProposal.proposalId = nanoid('23456789ABCDEFGHJKLMNPQRSTUVXYZ', 8);
+			await new Proposal(newProposal).save();
+			response.status(201).json(newProposal);
+		} catch(error) {
 			return response.status(400).json({
-				error: err.message
+				error: error.message
 			});
 		}
 	});
@@ -400,7 +476,13 @@ connection.once('open', () => {
 
 	const server = app.listen(process.env.PORT || 3000, function() {
 		const port = server.address().port;
-		console.log(Date.now() + "\nSERVICE_START:SUCCESS\nHOST: 127.0.0.1\nPORT: " + port + "\n");
+		console.log(
+			"\n" +
+			"STARTUP SUCCESS"
+			+ "\n" +
+			"SERVER RUNNING AT PORT " + port
+			+"\n"
+		);
 	});
 })
 ;
