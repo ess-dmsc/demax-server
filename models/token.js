2717043d4a15fe8bcb5aa8e0f31bef206223449b
Gareth Murphy <garethcmurphy@gmail.com>
2019-02-15 13:48:46 +0100

    add token

---
+++ models/token.js
@@ -0,0 +1,5 @@
+const tokenSchema = new mongoose.Schema({
+    _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    token: { type: String, required: true },
+    createdAt: { type: Date, required: true, default: Date.now, expires: 43200 }
+});
