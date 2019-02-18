1b3b331949334719de104dd88b17300752ea0c99
Gareth Murphy <garethcmurphy@gmail.com>
2019-02-18 10:03:01 +0100

    import mongoose

--- models/token.js
+++ models/token.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const tokenSchema = new mongoose.Schema({
     _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     token: { type: String, required: true },
