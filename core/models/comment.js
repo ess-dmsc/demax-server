9036e7a1e776b78d60311920d8a97ca3987a959d
jeremias.hillerberg <jeremias.hillerberg@esss.se>
2019-05-02 11:32:55 +0200

    comment

--- core/models/comment.js
+++ core/models/comment.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
+	commentId: String,
 	author: String,
 	comment: String,
+	proposalId: String,
 	timestamps: {createdAt: 'created_at', updatedAt: 'updated_at', deleteAt: 'delete_at'}
 });
 
