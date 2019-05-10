ee18583e5eed523ef9baa96a0ff66c23c5098f58
jeremias.hillerberg <jeremias.hillerberg@esss.se>
2019-05-10 12:07:07 +0200

    _

--- graphql_resolver.js
+++ graphql_resolver.js
@@ -2,16 +2,6 @@ const Proposal = require('./core/models/proposal.js';
 const User = require('./core/models/user.js';
 
 export const resolvers = {
-    Query: {
-        async allProposals() {
-            return await Proposal.find();
-        }
-    },
-     Query: {
-        async getAllUsers() {
-            return await User.find();
-        }
-    },
     Mutation: {
         async createProposal(root, {
             input
